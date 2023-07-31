96243cfadf0c6b5aa6db96b1c6828e66d43cc723
Moksh45 <mokshgupta567@gmail.com>
2023-07-31 22:44:22 +0530

    fix all the error

--- todoapp/models/user.js
+++ todoapp/models/user.js
@@ -1,23 +1,23 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        select:false,
-        minLength:[8,"Passwords must be at least 8 characters long"]
-    },
-}) 
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+    minLength: [6, "Password too short"],
+  },
+});
 
-mongoose.model = {};
+mongoose.models = {};
 
-export const User = mongoose.model("User", schema)
+export const User = mongoose.model("User", schema);
--- todoapp/pages/api/auth/register.js
+++ todoapp/pages/api/auth/register.js
@@ -1,36 +1,43 @@
-const { asyncError, errorHandler } = require("@/middlewares/error");
-import { User } from "../../../models/user"
-import { connectDB, cookieSetter, generateToken } from "@/utils/features";
-
+import { asyncError, errorHandler } from "../../../middlewares/error";
+import { User } from "../../../models/user";
+import {
+    connectDB,
+    cookieSetter,
+    generateToken,
+} from "../../../utils/features";
+import bcrypt from "bcrypt";
 
 const handler = asyncError(async (req, res) => {
+    if (req.method !== "POST")
+        return errorHandler(res, 400, "Only POST Method is allowed");
+
     const { name, email, password } = req.body;
 
-    if (!name || !email || !password) return errorHandler(res, 400, "Please Entet all fields");
+    if (!name || !email || !password)
+        return errorHandler(res, 400, "Please enter all fields");
 
     await connectDB();
 
-
     let user = await User.findOne({ email });
-    if (user) {
-        return errorHandler(res, 400, "User Already registered with this email")
-    }
+
+    if (user) return errorHandler(res, 400, "User registered with this email");
+
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     user = await User.create({
         name,
         email,
-        password,
+        password: hashedPassword,
     });
 
-    const token = generateToken(user._id)
+    const token = generateToken(user._id);
 
     cookieSetter(res, token, true);
 
     res.status(201).json({
-        success:true,
+        success: true,
         message: "Registered Successfully",
-    })
-
+    });
 });
 
 export default handler;
