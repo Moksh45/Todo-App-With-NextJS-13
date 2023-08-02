e9768f4a081176e78faa385dfb8f67598ad961c0
Moksh45 <mokshgupta567@gmail.com>
2023-08-02 23:05:52 +0530

    add chances

--- todoapp/app/register/page.jsx
+++ todoapp/app/register/page.jsx
@@ -1,14 +1,69 @@
 "use client";
-import Link from 'next/link'
-import React from 'react'
+
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import React, { useContext, useState } from "react";
+import { toast } from "react-hot-toast";
+import { Context } from "../../components/Clients";
+
 
 const page = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { user, setUser } = useContext(Context);
+
+    const registerHandler = async (e) => {
+        e.preventdefault();
+
+        try {
+            const res = await fetch("/api/auth/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    password,
+                }),
+            });
+
+            const data = await res.json();
+            if (!data.success) return toast.error(data.message);
+            setUser(data.user);
+            toast.success(data.message);
+        } catch (error) {
+            return toast.error(data.message);
+        }
+    }
+
+    if (user._id) return redirect("/");
+
+
     return <div className='login'>
         <section>
-            <form>
-                <input type='email' placeholder='Enter Email'/>
-                <input type='text' placeholder='Enter Name'/>
-                <input type='password' placeholder='Enter Password'/>
+            <form onSubmit={registerHandler}>
+                <input
+                    onChange={(e) => setName(e.target.value)}
+                    value={name}
+                    type="text"
+                    placeholder="Enter Name"
+                />
+                <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    type="email"
+                    placeholder="Enter Email"
+                />
+                <input
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    type="password"
+                    placeholder="Enter Password"
+                />
+
+
                 <button type='submit'>Sign Up</button>
 
                 <p>OR</p>
@@ -18,8 +73,4 @@ const page = () => {
     </div>
 }
 
-export const metadata = {
-    title: 'Register',
-    description: 'This is the Register page of Todo App Project',
-  }
-export default page
+export default page;
